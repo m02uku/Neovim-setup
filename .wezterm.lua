361279e7f9ce385cee7d1690104bb31a4896abb5
nagano <157340743+m02uku@users.noreply.github.com>
2024-10-07 16:13:33 +0900

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Dracula (Official)'
+config.color_scheme = 'Dracula'
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = true
 config.font_size = 10
